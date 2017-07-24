bf8e90ae464ae839ef8a80f89c9012af7840a15b
waystogetthere <30395015+waystogetthere@users.noreply.github.com>
2017-07-24 20:46:24 +0800

    New functions:ignore the dates,saving positions
    This code add two new functions in the demo cpp source.
    New functions:
    1)Since the video downloaded from dh HDVR would show time at the top right corner.So we ignore this part when we detect dolphins.
    2)We can save dolphins' positions in a file now.

--- examples/detect_dolphins.cpp
+++ examples/detect_dolphins.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
+#include<fstream>
 
 #ifdef __linux__
 #define PRINTF printf
@@ -19,15 +20,16 @@ using namespace std;
 int main()
 {
 	vector<Rect> bboxes;
-	char path[100];
+	char path[100]; 
+	fstream dolphin;
 
 	PRINTF("please input image/video path: ");
-	SCANF("%s", path);
+	SCANF("%s", path);  
 	int path_len = strlen(path);
-	if (path_len < 4) {
+	if (path_len < 4) { 
 		PRINTF("invalid path\n");
 		return 1;
-	}
+	} 
 	if (strcmp(path + path_len - 3, "jpg") == 0 ||
 		strcmp(path + path_len - 3, "png") == 0 ||
 		strcmp(path + path_len - 4, "jpeg") == 0) {
@@ -45,17 +47,22 @@ int main()
 		imshow("image", img);
 		waitKey();
 	} else if (strcmp(path + path_len - 3, "avi") == 0 ||
-		strcmp(path + path_len - 3, "mp4") == 0){
+		strcmp(path + path_len - 3, "mp4") == 0 || strcmp(path + path_len - 3, "txt") == 0)
+		{
 		PRINTF("get video input: %s\n", path);
 		VideoCapture capture;
 		capture.open(path);
 		if (!capture.isOpened()) {
 			PRINTF("fail to read: %s\n", path);
+			system("pause");
 			return 1;
 		}
 		Mat frame;
 		int frame_id = 0;
+
 		while (capture.read(frame)) {
+			//以下定义了一个变量获取当前帧时间
+			int now = capture.get(CV_CAP_PROP_POS_MSEC);
 			resize(frame, frame, Size(960, 540), (0, 0), (0, 0), cv::INTER_LINEAR);
 			if (!frame.data) {
 				PRINTF("done\n");
@@ -65,10 +72,28 @@ int main()
 			doldetect(frame, bboxes, 400, 0.5, 40, 3);
       PRINTF("[frame %d] %d\n", frame_id, int(bboxes.size()));
 			for (size_t i = 0; i < bboxes.size(); ++i) {
+
+				if (bboxes[i].x > 903 || bboxes[i].y>48 || bboxes[i].x + bboxes[i].width<730 || bboxes[i].y + bboxes[i].height<11)
 				rectangle(frame, Point(bboxes[i].x, bboxes[i].y),
 					Point(bboxes[i].x + bboxes[i].width, bboxes[i].y + bboxes[i].height), Scalar(0, 255, 0));
 			}
+			//以下是保存当前帧的海豚个数，以及每个海豚对应的坐标
+			if (bboxes.size() != 0)
+			{
+				dolphin.open("dolphin.txt", ios::ate | ios::out|ios::app);
+				dolphin << "The time is: " << now << " ms" << endl;
+				dolphin << "The number :" << bboxes.size() << endl;
+				for (size_t i = 0; i < bboxes.size(); ++i)
+				{
+					dolphin << "The " << i + 1 << "th's center is at x: " << bboxes[i].x + bboxes[i].width / 2 << " , y: " << bboxes[i].y + bboxes[i].height / 2 << endl; 
+					dolphin << "x:" << bboxes[i].x << ",y: " << bboxes[i].y << ",width: " << bboxes[i].width << ",height: " << bboxes[i].height << endl;
+				}
+				dolphin << endl;
+				dolphin.close();
+			}
+
 			imshow("video", frame);
+			
 			waitKey(10);
 			++frame_id;
 		}
